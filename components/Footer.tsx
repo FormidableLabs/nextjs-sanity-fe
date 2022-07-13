16c32d92289bf9fa4b2f6dc1adbf7aed4adb1ad5
Binoy <binoy.patel@formidable.com>
2022-07-12 21:24:24 -0400

    Updates to category and page layout and setup Groq (#3)

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,3 @@
 export const Footer = () => {
-  return <div className="h-24 border-t flex items-center justify-start px-6">© MMXXII Formidable Labs, LLC.</div>;
+  return <div className="h-10 border-t flex items-center justify-start px-6">© MMXXII Formidable Labs, LLC.</div>;
 };
