e06c7a3b73c17f46496e269a86fa57cd0851c592
Scott Nguyen <scottqmn@gmail.com>
2022-08-02 16:57:56 -0700

    Multiple options

--- packages/nextjs/components/ProductFilters/ProductFilters.tsx
+++ packages/nextjs/components/ProductFilters/ProductFilters.tsx
@@ -7,14 +7,19 @@ type ProductFiltersProps = {
   filtersData?: FiltersGroup[];
 };
 
+const isString = (el: any) => {
+  return typeof el === "string" || el instanceof String;
+};
+
 const DUMMY_DATA = [
   {
     id: 1,
     name: "promotion",
-    label: "Shop by Promotion",
+    label: "Promotion",
     options: [
       { id: 1, name: "sale", label: "Sale" },
       { id: 2, name: "bogo", label: "BOGO" },
+      { id: 3, name: "2day", label: "Free 2-day Shipping" },
     ],
   },
 ];
@@ -22,15 +27,37 @@ const DUMMY_DATA = [
 export const ProductFilters: React.FC<ProductFiltersProps> = ({ filtersData = DUMMY_DATA }) => {
   const router = useRouter();
 
-  const handleChange = (group: string, option: string) => (e: ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>, group: string, option: string) => {
+    const currentValue = router.query[group];
+
     if (e?.target?.checked) {
-      router.replace({
-        query: { ...router.query, [group]: option },
-      });
+      // Add option to query params
+      if (currentValue) {
+        if (isString(currentValue)) {
+          // Single existing, create and append to array
+          router.query[group] = [currentValue as string, option];
+        } else {
+          // Multiple options, append to array
+          (router.query[group] as string[]).push(option);
+        }
+      } else {
+        // No existing options, sdd single option
+        router.query[group] = option;
+      }
     } else {
-      // Remove
-      //   params;
+      // Remove option from query params
+      if (currentValue) {
+        if (isString(currentValue)) {
+          // Single option, remove group and option
+          delete router.query[group];
+        } else {
+          // Multiple options, only remove option
+          router.query[group] = (router.query[group] as string[]).filter((value) => value !== option);
+        }
+      }
     }
+
+    router.replace({ query: router.query });
   };
 
   return (
@@ -44,16 +71,23 @@ export const ProductFilters: React.FC<ProductFiltersProps> = ({ filtersData = DU
               <ul>
                 {options.map(({ id: optionId, name: optionName, label: optionLabel }) => {
                   const query = router.query?.[groupName];
-                  const queryIsString = typeof query === "string" || query instanceof String;
+                  const queryIsString = isString(query);
 
                   const isChecked =
-                    !!query &&
-                    ((queryIsString && query === optionName) || (!queryIsString && query.includes(optionName)));
+                    !!query && // Value exists
+                    ((queryIsString && query === optionName) || // Single value matching option
+                      (!queryIsString && query.includes(optionName))); // Multiple values including option
 
                   return (
                     <li key={optionId}>
                       <label>
-                        <input type="checkbox" checked={isChecked} onChange={handleChange(groupName, optionName)} />
+                        <input
+                          type="checkbox"
+                          checked={isChecked}
+                          onChange={(e) => {
+                            handleChange(e, groupName, optionName);
+                          }}
+                        />
                         <span>{optionLabel}</span>
                       </label>
                     </li>
