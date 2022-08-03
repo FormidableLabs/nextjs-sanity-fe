d144f66b2da963d808c6c63897bfae76bb877c57
Luis Abarca <luisabarca@gmail.com>
2022-08-03 12:46:13 -0500

    update pagination links to use Link component

--- packages/nextjs/components/Pagination.tsx
+++ packages/nextjs/components/Pagination.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 const pageSelectedClass = "border-indigo-500 text-indigo-600 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium";
@@ -28,13 +29,12 @@ export const Pagination = ({ onPageChange, pageCount = 1, currentPage = 1, baseU
             <div className="-mt-px w-0 flex-1 flex">
             {
                 currentPage > 1 ? (
-                <a
-                    href={prevUrl}
-                    className="border-t-2 border-transparent pt-4 pr-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300"
-                >
-                    <FaChevronLeft className="mr-3 h-5 w-5 text-gray-400" aria-hidden="true" />
-                    Previous
-                </a>
+                    <Link href={prevUrl} passHref>
+                        <a className="border-t-2 border-transparent pt-4 pr-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300">
+                            <FaChevronLeft className="mr-3 h-5 w-5 text-gray-400" aria-hidden="true" />
+                            Previous
+                        </a>
+                    </Link>
                 ) : (
                     <span className="border-t-2 border-transparent pt-4 pr-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300">
                         <FaChevronLeft className="mr-3 h-5 w-5 text-gray-400" aria-hidden="true" />
@@ -46,14 +46,15 @@ export const Pagination = ({ onPageChange, pageCount = 1, currentPage = 1, baseU
             <div className="hidden md:-mt-px md:flex">
             {
                 totalPages.map((page) => (
-                    <a key={`page-${page}`}
-                        onClick={(e) => handlePageChanged(e, page)}
-                        href={`${baseUrl}${page === 1 ? "" : `?page=${page}`}`}
-                        className={page == currentPage ? pageSelectedClass : pageClass}
-                        aria-current={page === currentPage ? "page" : "false"}
-                    >
-                        {page}
-                    </a>
+                    <Link key={`page-${page}`} href={`${baseUrl}${page === 1 ? "" : `?page=${page}`}`} passHref>
+                        <a
+                            onClick={(e) => handlePageChanged(e, page)}
+                            className={page == currentPage ? pageSelectedClass : pageClass}
+                            aria-current={page === currentPage ? "page" : "false"}
+                        >
+                            {page}
+                        </a>
+                    </Link>
                 ))
             }
             
@@ -61,13 +62,12 @@ export const Pagination = ({ onPageChange, pageCount = 1, currentPage = 1, baseU
             <div className="-mt-px w-0 flex-1 flex justify-end">
             {
                 currentPage < pageCount ? (
-                    <a
-                        href={nextUrl}
-                        className="border-t-2 border-transparent pt-4 pl-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300"
-                    >
-                        Next
-                        <FaChevronRight className="ml-3 h-5 w-5 text-gray-400" aria-hidden="true" />
-                    </a>
+                    <Link href={nextUrl} passHref>
+                        <a className="border-t-2 border-transparent pt-4 pl-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300">
+                            Next
+                            <FaChevronRight className="ml-3 h-5 w-5 text-gray-400" aria-hidden="true" />
+                        </a>
+                    </Link>
                 ) : (
                     <span className="border-t-2 border-transparent pt-4 pl-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300">
                         Next
