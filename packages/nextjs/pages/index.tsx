ae980cfa0d6b2d9f8d6bf1c12ac415a8da59725b
Luis Abarca <luisabarca@gmail.com>
2022-08-03 11:52:15 -0500

    update index page to reuse categories server side props

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,9 +1,9 @@
-import { GetServerSideProps, NextPage } from "next";
+import { NextPage } from "next";
 import { withUrqlClient } from "next-urql";
-
-import { Card } from "../components/Card";
-import { GetCategoriesDocument, useGetCategoriesQuery } from "../utils/generated/graphql";
-import { initializeUrql, urqlOptions, withUrqlOptions } from "../utils/urql";
+import { useGetCategoriesQuery } from "utils/generated/graphql";
+import { urqlOptions, withUrqlOptions } from "utils/urql";
+import { CategoryList } from "components/CategoryList";
+import { getServerSideProps as categoriesServerSideProps } from "./categories";
 
 const Home: NextPage = () => {
   const [{ data }] = useGetCategoriesQuery();
@@ -11,38 +11,11 @@ const Home: NextPage = () => {
   return (
     <div className="m-4">
       <h3 className="text-lg text-center font-bold my-5">Top Categories</h3>
-      <ul className="flex justify-evenly">
-        {data?.allCategory.map((category) => (
-          <li key={category._id}>
-            <Card
-              to={`/categories/${category.slug?.current}`}
-              imageProps={{
-                src: category.images?.[0]?.images ?? "",
-                alt: category.images?.[0]?.name ?? "",
-              }}
-            >
-              {category.name}
-            </Card>
-          </li>
-        ))}
-      </ul>
+      <CategoryList items={data?.allCategory} />
     </div>
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const { client, ssrCache } = initializeUrql();
-
-  // This query is used to populate the cache for the query
-  // used on this page.
-  await client?.query(GetCategoriesDocument).toPromise();
-
-  return {
-    props: {
-      // urqlState is a keyword here so withUrqlClient can pick it up.
-      urqlState: ssrCache.extractData(),
-    },
-  };
-};
+export const getServerSideProps = categoriesServerSideProps;
 
 export default withUrqlClient(() => ({ ...urqlOptions }), withUrqlOptions)(Home);
