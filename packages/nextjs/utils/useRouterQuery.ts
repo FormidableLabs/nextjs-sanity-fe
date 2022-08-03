c9b979b5b72ee1c2dcc7fbeb68e64b06384205a3
Scott Nguyen <scottqmn@gmail.com>
2022-08-03 12:02:37 -0700

    Hook for modifiying query params

---
+++ packages/nextjs/utils/useRouterQuery.ts
@@ -0,0 +1,48 @@
+import { useRouter } from "next/router";
+
+export const useRouterQueryParams = () => {
+  const router = useRouter();
+
+  const add = (key: string, value: string) => {
+    const currentValue = router.query[key];
+    if (currentValue) {
+      if (typeof currentValue === "string" || currentValue instanceof String) {
+        // Single existing, create and append to array
+        router.query[key] = [currentValue as string, value];
+      } else {
+        // Multiple options, append to array
+        (router.query[key] as string[]).push(value);
+      }
+    } else {
+      // No existing options, sdd single option
+      router.query[key] = value;
+    }
+    router.replace({ query: router.query });
+  };
+
+  const replace = (key: string, value: string) => {
+    router.query[key] = value;
+    router.replace({ query: router.query });
+  };
+
+  const remove = (key: string, value: string) => {
+    const currentValue = router.query[key];
+    if (currentValue) {
+      if (typeof currentValue === "string" || currentValue instanceof String) {
+        // Single option, remove group and option
+        delete router.query[key];
+      } else {
+        // Multiple options, only remove option
+        router.query[key] = (router.query[key] as string[]).filter((curr) => curr !== value);
+      }
+    }
+    router.replace({ query: router.query });
+  };
+
+  const clear = (key: string) => {
+    delete router.query[key];
+    router.replace({ query: router.query });
+  };
+
+  return { add, replace, remove, clear };
+};
