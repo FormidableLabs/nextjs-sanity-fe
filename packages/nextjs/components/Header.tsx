6fffad1b117dd45f0a02d7654bd889db18829b7b
Binoy <binoy.patel@formidable.com>
2022-08-03 13:52:24 -0400

    Setup Cart API and Add to Cart functionality (#22)
    * Cart API and WIP Cart FE
    
    * Connect Cart FE to the Cart API
    
    * Remove unnecessary setState logic
    
    * Use API response for FE Cart

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -1,14 +1,22 @@
 import Link from "next/link";
 import React from "react";
+import { MdShoppingCart } from "react-icons/md";
+
+import { useCart } from "./CartContext";
 
 export const Header = () => {
+  const { cart } = useCart();
+
   return (
-    <div className="h-20 flex items-center bg-white px-6 border-b shadow">
+    <div className="h-20 flex items-center justify-between bg-white px-6 border-b shadow">
       <h2 className="text-2xl font-bold">
         <Link href="/">
           <a>NextJs Ecom</a>
         </Link>
       </h2>
+      <div className="flex items-center">
+        <MdShoppingCart size={30} /> <span className="text-gray-600 text-xl">{Object.keys(cart).length}</span>
+      </div>
     </div>
   );
 };
