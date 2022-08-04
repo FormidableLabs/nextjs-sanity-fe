c2dab2aa55673391db88c99d659f6345daf0410f
Scott Nguyen <scottqmn@gmail.com>
2022-08-04 11:31:25 -0700

    Use icons package

--- packages/nextjs/components/ImageCarousel/ImageCarouselNext.tsx
+++ packages/nextjs/components/ImageCarousel/ImageCarouselNext.tsx
@@ -1,5 +1,7 @@
 import type { RenderControlProps } from "./types";
 
+import { HiOutlineArrowCircleRight } from "react-icons/hi";
+
 export const ImageCarouselNext = ({ nextSlide, slideCount, currentSlide }: RenderControlProps) => (
   <button
     type="button"
@@ -8,19 +10,6 @@ export const ImageCarouselNext = ({ nextSlide, slideCount, currentSlide }: Rende
     disabled={currentSlide === slideCount - 1}
     className="p-4 disabled:opacity-70"
   >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-6 w-6"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      strokeWidth={2}
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
-      />
-    </svg>
+    <HiOutlineArrowCircleRight className="w-6 h-auto" />
   </button>
 );
--- packages/nextjs/components/ImageCarousel/ImageCarouselPrev.tsx
+++ packages/nextjs/components/ImageCarousel/ImageCarouselPrev.tsx
@@ -1,5 +1,7 @@
 import type { RenderControlProps } from "./types";
 
+import { HiOutlineArrowCircleLeft } from "react-icons/hi";
+
 export const ImageCarouselPrev = ({ previousSlide, currentSlide }: RenderControlProps) => (
   <button
     type="button"
@@ -8,19 +10,6 @@ export const ImageCarouselPrev = ({ previousSlide, currentSlide }: RenderControl
     disabled={currentSlide === 0}
     className="p-4 disabled:opacity-70"
   >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-6 w-6"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      strokeWidth={2}
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z"
-      />
-    </svg>
+    <HiOutlineArrowCircleLeft className="w-6 h-auto" />
   </button>
 );
