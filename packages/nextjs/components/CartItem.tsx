0bfeb9380ca16e6e3cc803300f6f898a5244126c
Scott Nguyen <scottqmn@gmail.com>
2022-08-09 10:32:44 -0700

    cart item layout with placeholder data

--- packages/nextjs/components/CartItem.tsx
+++ packages/nextjs/components/CartItem.tsx
@@ -1,5 +1,6 @@
 import { HiMinusSm, HiPlusSm, HiOutlineTrash } from "react-icons/hi";
 import { useCart } from "./CartContext";
+import { Image } from "./Image";
 
 type CartItemProps = {
   id: string;
@@ -13,45 +14,95 @@ export const CartItem: React.FC<CartItemProps> = ({ id, qty }) => {
     updateCart(id, value);
   };
 
-  return (
-    <div className="flex gap-4">
-      {/* TODO: get product name */}
-      <div>id: {id}</div>
-      <div className="flex border-solid border-2 border-slate-500 divide-x rounded">
-        <button
-          type="button"
-          onClick={() => {
-            updateQty(-1);
-          }}
-          disabled={qty <= 1}
-          aria-label="Decrease quantity"
-          className="px-1 disabled:opacity-50"
-        >
-          <HiMinusSm />
-        </button>
-        <div className="w-8 px-1 text-center">{qty}</div>
-        <button
-          type="button"
-          onClick={() => {
-            updateQty(1);
-          }}
-          aria-label="Increase quantity"
-          className="px-1"
-        >
-          <HiPlusSm />
-        </button>
-      </div>
+  // TODO: query actual data from id
+  const item = {
+    slug: { current: "blank-t-shirt" },
+    images: [
+      {
+        images: {
+          asset: {
+            _id: "image-f7d23d0907c178c204e8fded03b8781ba581638c-3000x3000-jpg",
+            __typename: "SanityImageAsset",
+          },
+          __typename: "Image",
+        },
+        name: "Plain white blank T-Shirt",
+        __typename: "ProductImage",
+      },
+      {
+        images: {
+          asset: {
+            _id: "image-71a1ad84e14f2a7e440c3f813798ea8aac8ef15d-2392x2392-jpg",
+            __typename: "SanityImageAsset",
+          },
+          __typename: "Image",
+        },
+        name: "Plain white blank T-shirt (side)",
+        __typename: "ProductImage",
+      },
+      {
+        images: {
+          asset: {
+            _id: "image-6693427ebf4148318be477e644349f74e08d8999-1302x1302-jpg",
+            __typename: "SanityImageAsset",
+          },
+          __typename: "Image",
+        },
+        name: "Plain white blank T-shirt (product)",
+        __typename: "ProductImage",
+      },
+    ],
+    name: "Blank T-shirt",
+    msrp: 30,
+    price: 15,
+  };
 
-      <button
-        type="button"
-        onClick={() => {
-          updateQty(0);
-        }}
-        aria-label="Delete item"
-        className="px-1 border-solid border-2 border-slate-500 rounded"
-      >
-        <HiOutlineTrash />
-      </button>
+  return (
+    <div className="flex gap-8 py-4">
+      <div className="w-24 aspect-square bg-slate-100">
+        <Image width={96} height={96} src={item?.images?.[0]?.images ?? ""} alt={item?.images?.[0]?.name ?? ""} />
+      </div>
+      <div className="flex-auto">
+        <div>{item.name}</div>
+        {/* Quantity */}
+        <div className="flex gap-2 mt-2">
+          <div className="flex border-solid border-2 border-slate-500 divide-x rounded">
+            <button
+              type="button"
+              onClick={() => {
+                updateQty(-1);
+              }}
+              disabled={qty <= 1}
+              aria-label="Decrease quantity"
+              className="px-1 disabled:opacity-50"
+            >
+              <HiMinusSm />
+            </button>
+            <div className="w-8 px-1 text-center">{qty}</div>
+            <button
+              type="button"
+              onClick={() => {
+                updateQty(1);
+              }}
+              aria-label="Increase quantity"
+              className="px-1"
+            >
+              <HiPlusSm />
+            </button>
+          </div>
+          <button
+            type="button"
+            onClick={() => {
+              updateQty(0);
+            }}
+            aria-label="Delete item"
+            className="px-1 border-solid border-2 border-slate-500 rounded"
+          >
+            <HiOutlineTrash />
+          </button>
+        </div>
+      </div>
+      <div>{new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(item.price * qty)}</div>
     </div>
   );
 };
--- packages/nextjs/pages/cart.tsx
+++ packages/nextjs/pages/cart.tsx
@@ -1,7 +1,9 @@
 import { useCart } from "../components/CartContext";
 import { CartItem } from "../components/CartItem";
+import { GetProductDocument, GetProductQuery, Maybe, useGetProductsQuery } from "../utils/generated/graphql";
 
 import type { NextPage } from "next";
+import Link from "next/link";
 
 interface Props {
   cartItems: Record<string, any>;
@@ -16,8 +18,8 @@ const CartPage: NextPage<Props> = (props) => {
 
   return (
     <div className="h-full mb-4">
-      <h1 className="text-2xl font-bold m-4">Cart</h1>
-      <ul>
+      <h1 className="text-2xl font-bold my-4">Cart</h1>
+      <ul className="flex flex-col gap-4">
         {cartArray.map(({ id, qty }) => (
           <li key={id}>
             <CartItem id={id} qty={qty} />
@@ -25,14 +27,27 @@ const CartPage: NextPage<Props> = (props) => {
         ))}
       </ul>
 
-      {cartArray.length > 0 && (
-        <button
-          type="button"
-          onClick={clearCart}
-          className="border-solid border-2 border-slate-500 px-2 rounded disabled:opacity-50"
-        >
-          Clear Cart
-        </button>
+      {cartArray.length < 0 ? (
+        <div className="flex justify-between gap-4 mt-6">
+          <button
+            type="button"
+            onClick={clearCart}
+            className="block px-6 py-2 border-solid border-2 border-slate-500 rounded"
+          >
+            Clear Cart
+          </button>
+
+          <Link href="/checkout">
+            <a className="w-fit block px-6 py-2 border-solid border-2 border-slate-500 rounded">Checkout</a>
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <div className="text-xl">You have nothing in your cart</div>
+          <Link href="/">
+            <a className="w-fit block mt-4 px-6 py-2 border-solid border-2 border-slate-500 rounded">View Products</a>
+          </Link>
+        </div>
       )}
     </div>
   );
