6052057022751021e5901beb1fd050f569372a48
Scott Nguyen <scottqmn@gmail.com>
2022-08-09 14:24:21 -0700

    Clone query instead of mutating directly

--- packages/nextjs/utils/useRouterQueryParams.ts
+++ packages/nextjs/utils/useRouterQueryParams.ts
@@ -4,44 +4,47 @@ export const useRouterQueryParams = () => {
   const router = useRouter();
 
   const add = (key: string, value: string) => {
-    const currentValue = router.query[key];
+    const query = { ...router.query };
+    const currentValue = query[key];
     if (currentValue) {
       if (typeof currentValue === "string" || currentValue instanceof String) {
         // Single existing, create and append to array
-        router.query[key] = [currentValue as string, value];
+        query[key] = [currentValue as string, value];
       } else {
         // Multiple options, append to array
-        (router.query[key] as string[]).push(value);
+        (query[key] as string[]).push(value);
       }
     } else {
       // No existing options, sdd single option
-      router.query[key] = value;
+      query[key] = value;
     }
-    router.replace({ query: router.query });
+    router.replace({ query });
   };
 
   const replace = (key: string, value: string) => {
-    router.query[key] = value;
-    router.replace({ query: router.query });
+    const query = { ...router.query, [key]: value };
+    router.replace({ query });
   };
 
   const clear = (key: string) => {
-    delete router.query[key];
-    router.replace({ query: router.query });
+    const query = { ...router.query };
+    delete query[key];
+    router.replace({ query });
   };
 
   const remove = (key: string, value: string) => {
-    const currentValue = router.query[key];
+    const query = { ...router.query };
+    const currentValue = query[key];
     if (currentValue) {
       if (typeof currentValue === "string" || currentValue instanceof String) {
         // Single option, remove group and option
-        delete router.query[key];
+        delete query[key];
       } else {
         // Multiple options, only remove option
-        router.query[key] = (router.query[key] as string[]).filter((curr) => curr !== value);
+        query[key] = (query[key] as string[]).filter((curr) => curr !== value);
       }
     }
-    router.replace({ query: router.query });
+    router.replace({ query });
   };
 
   return { add, replace, clear, remove, query: router.query };
