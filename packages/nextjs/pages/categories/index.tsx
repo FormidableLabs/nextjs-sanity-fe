d89456b2829de62f6979cd886202215e3ed73208
Sawyer Burnett <sawyerburnett@gmail.com>
2022-08-10 08:38:16 -0700

    fix reference

--- packages/nextjs/pages/categories/index.tsx
+++ packages/nextjs/pages/categories/index.tsx
@@ -4,7 +4,7 @@ import { withUrqlClient } from "next-urql";
 import { useGetCategoriesQuery } from "utils/generated/graphql";
 import { urqlOptions, withUrqlOptions } from "utils/urql";
 import { CategoryList } from "components/CategoryList";
-import { getCategoryServerSideProps } from "pages/categories/getCategoryServerSideProps";
+import { getCategoryServerSideProps } from "utils/getCategoryServerSideProps";
 
 const CategoriesPage: NextPage = () => {
   // This component is essentially a copy paste from pages/index.tsx. Do we need this duplication?
