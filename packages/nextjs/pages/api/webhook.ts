0c6e2d3b6c0780df9f0d4010b9fbd67396b2288f
Sawyer Burnett <sawyerburnett@gmail.com>
2022-08-11 14:46:34 -0700

    cleaning up some things

--- packages/nextjs/pages/api/webhook.ts
+++ packages/nextjs/pages/api/webhook.ts
@@ -3,35 +3,46 @@ import { isValidSignature, SIGNATURE_HEADER_NAME } from "@sanity/webhook";
 
 const secret = process.env.SANITY_WEBHOOK_SECRET ?? "";
 
+/**
+ * This is based off of the projection set up in the webhook
+  {
+    _id,
+    _type,
+    slug
+  } 
+ */
+interface WebhookPayload {
+  _id: string;
+  slug: {
+    current: string;
+    _type: string;
+  };
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   // Auth example pulled from https://github.com/sanity-io/webhook-toolkit
   const signatureHeader = req.headers[SIGNATURE_HEADER_NAME] ?? "";
   const signature = Array.isArray(signatureHeader) ? signatureHeader[0] : signatureHeader;
   const body = await readBody(req); // Read the body into a string
-  console.log("parsed body: ");
-  console.log(body);
+  console.log("received a potential webhook request");
 
   if (!isValidSignature(body, signature, secret)) {
     console.log("unauthorized webhook attempt thwarted");
     res.status(401).json({ success: false, message: "Unauthorized" });
     return;
   }
+  console.log("authorization step has passed");
 
-  console.log("validation has passed!");
+  req.body = JSON.parse(body) as WebhookPayload; // since body parse is turned off, we have to manually replace req.body
+  console.log("parsed body: ");
+  console.log(req.body);
 
-  console.log("received a potential webhook request with the following payload: ");
-
-  // validate request
   // send purge request to Fastly
   // get fastly token to use in purge request
   // set environment variables in CI rather than manually
 
-  console.log(req.body);
   res.status(200).json({
-    message: "hello, webhook!",
-    body: req.body,
-    query: req.query,
-    cookies: req.cookies,
+    message: "Webhook received",
   });
 }
 
