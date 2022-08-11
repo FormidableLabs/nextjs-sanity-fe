880d9c85e344c9f151d10c18fd59c9ea3f2040ba
Sawyer Burnett <sawyerburnett@gmail.com>
2022-08-11 09:48:54 -0700

    add setCachingHeaders util

---
+++ packages/nextjs/utils/setCachingHeaders.ts
@@ -0,0 +1,15 @@
+import { ServerResponse } from "http";
+
+/**
+ * sets appropriate caching response header, along with an optional set of surrogate keys
+ * note: when running `next dev`, these headers are overwritten to prevent local caching
+ * @param res
+ * @param keys
+ */
+export const setCachingHeaders = (res: ServerResponse, keys?: string[]): void => {
+  res.setHeader("Cache-Control", "public, max-age=0");
+  res.setHeader("Surrogate-Control", "max-age=600, stale-while-revalidate=120, stale-if-error=600");
+  if (keys && keys.length > 0) {
+    res.setHeader("Surrogate-Key", keys.join(" "));
+  }
+};
