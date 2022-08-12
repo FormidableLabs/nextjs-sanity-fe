c4721ccf4635627f18e1876d852be9d35f9ad459
Sawyer Burnett <sawyerburnett@gmail.com>
2022-08-12 09:50:33 -0700

    add more webhook functionality and validations

--- packages/nextjs/pages/api/webhook.ts
+++ packages/nextjs/pages/api/webhook.ts
@@ -1,49 +1,70 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { isValidSignature, SIGNATURE_HEADER_NAME } from "@sanity/webhook";
 
+const fastlyApiKey = process.env.FASTLY_API_KEY ?? "";
+const fastlyServiceId = process.env.FASTLY_SERVICE_ID ?? "";
 const secret = process.env.SANITY_WEBHOOK_SECRET ?? "";
 
 /**
  * This is based off of the projection set up in the webhook
-  {
-    _id,
-    _type,
-    slug
-  } 
+ * 
+    *[_type in ["category", "product"]]
+    {
+        _id,
+        _type,
+        'slug': slug.current
+    }
+ *   
  */
 interface WebhookPayload {
   _id: string;
-  slug: {
-    current: string;
-    _type: string;
-  };
+  slug: string;
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // Auth example pulled from https://github.com/sanity-io/webhook-toolkit
-  const signatureHeader = req.headers[SIGNATURE_HEADER_NAME] ?? "";
-  const signature = Array.isArray(signatureHeader) ? signatureHeader[0] : signatureHeader;
-  const body = await readBody(req); // Read the body into a string
-  console.log("received a potential webhook request");
+  try {
+    // only allow post requests
+    if (req.method !== "POST") {
+      res.status(405).json({ message: "Only POST is supported" });
+      return;
+    }
 
-  if (!isValidSignature(body, signature, secret)) {
-    console.log("unauthorized webhook attempt thwarted");
-    res.status(401).json({ success: false, message: "Unauthorized" });
-    return;
+    // Auth example pulled from https://github.com/sanity-io/webhook-toolkit
+    const signatureHeader = req.headers[SIGNATURE_HEADER_NAME] ?? "";
+    const signature = Array.isArray(signatureHeader) ? signatureHeader[0] : signatureHeader;
+    const body = await readBody(req); // Read the body into a string
+
+    if (!body) {
+      res.status(400).json({ message: "Bad Input" });
+      return;
+    }
+    console.log("received a potential webhook request");
+
+    if (!isValidSignature(body, signature, secret)) {
+      console.log("unauthorized webhook attempt thwarted");
+      res.status(401).json({ success: false, message: "Unauthorized" });
+      return;
+    }
+    console.log("authorization step has passed");
+
+    // req.body is empty because bodyParser has been disabled below
+    const parsedBody = JSON.parse(body) as WebhookPayload;
+    console.log("parsed body: ");
+    console.log(parsedBody);
+
+    const { slug } = parsedBody;
+
+    await requestPurge([slug]);
+
+    res.status(200).json({
+      message: "Webhook processed successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Webhook failed to process",
+    });
   }
-  console.log("authorization step has passed");
-
-  req.body = JSON.parse(body) as WebhookPayload; // since body parse is turned off, we have to manually replace req.body
-  console.log("parsed body: ");
-  console.log(req.body);
-
-  // send purge request to Fastly
-  // get fastly token to use in purge request
-  // set environment variables in CI rather than manually
-
-  res.status(200).json({
-    message: "Webhook received",
-  });
 }
 
 // Next.js will by default parse the body, which can lead to invalid signatures
@@ -52,6 +73,26 @@ export const config = {
     bodyParser: false,
   },
 };
+
+async function requestPurge(keys: string[]): Promise<boolean> {
+  const response = await fetch(`https://api.fastly.com/service/${fastlyServiceId}/purge`, {
+    method: "POST",
+    headers: {
+      "Fastly-Key": fastlyApiKey,
+      "surrogate-key": keys.join(" "),
+    },
+  });
+
+  // 'ok' means an http status in 200 range
+  if (response.ok) {
+    const data = await response.json();
+    console.log("cache purge succesfully requested", data);
+    return true;
+  } else {
+    console.log("error occurred when calling the Fastly purge api");
+    return false;
+  }
+}
 
 async function readBody(readable: NextApiRequest) {
   const chunks = [];
