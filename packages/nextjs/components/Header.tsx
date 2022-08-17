ee0b4b356e436e8986b91d9b98eae9a05c7610b9
Binoy <binoy.patel@formidable.com>
2022-08-16 23:07:02 -0400

    Navigation for all products and blogs (#52)

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -4,16 +4,36 @@ import { MdShoppingCart } from "react-icons/md";
 
 import { useCart } from "./CartContext";
 
+const NAV_ITEMS = [
+  {
+    href: "/products",
+    label: "Products",
+  },
+  {
+    href: "/blogs",
+    label: "Blogs",
+  },
+];
+
 export const Header = () => {
   const { cartTotal } = useCart();
 
   return (
     <div className="h-20 flex items-center justify-between bg-white px-6 border-b shadow">
-      <h2 className="text-2xl font-bold">
-        <Link href="/">
-          <a>NextJs Ecom</a>
-        </Link>
-      </h2>
+      <div className="flex items-center">
+        <h2 className="text-2xl font-bold">
+          <Link href="/">
+            <a>NextJs Ecom</a>
+          </Link>
+        </h2>
+        <nav className="ml-8">
+          {NAV_ITEMS.map(({ href, label }) => (
+            <Link href={href} key={label}>
+              <a className="text-gray-600 text-xl hover:text-gray-900 mr-2">{label}</a>
+            </Link>
+          ))}
+        </nav>
+      </div>
       <Link href="/cart">
         <a className="flex items-center">
           <MdShoppingCart size={30} /> <span className="text-gray-600 text-xl">{cartTotal}</span>
