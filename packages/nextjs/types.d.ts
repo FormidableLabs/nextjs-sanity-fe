cc0e57ae0ee5ac9679853b65153ca57feea7d088
Binoy Patel <binoy.patel@formidable.com>
2022-08-25 15:37:06 -0400

    Remove use of env var for cookie name

--- packages/nextjs/types.d.ts
+++ packages/nextjs/types.d.ts
@@ -2,7 +2,6 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     readonly NEXT_PUBLIC_SANITY_GRAPHQL_URL: string;
     readonly NEXT_PUBLIC_SANITY_PROJECT_ID: string;
-    readonly NEXT_PUBLIC_CART_COOKIE_NAME: string;
     readonly NEXT_PUBLIC_PAGINATION_PAGE_SIZE: number;
   }
 }
--- packages/nextjs/utils/apiHelpers/cartCookie.ts
+++ packages/nextjs/utils/apiHelpers/cartCookie.ts
@@ -1,7 +1,7 @@
 import { serialize } from "cookie";
 import { NextApiRequest, NextApiResponse } from "next";
 
-const CART_COOKIE_NAME = process.env.NEXT_PUBLIC_CART_COOKIE_NAME;
+const CART_COOKIE_NAME = "formidable-cart";
 
 export function getCookie(cookies: NextApiRequest["cookies"]) {
   return cookies[CART_COOKIE_NAME] ? JSON.parse(cookies[CART_COOKIE_NAME]) : {};
