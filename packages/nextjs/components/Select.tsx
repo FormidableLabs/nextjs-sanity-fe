20c388ab8bbc21660012b1d93e2553236d8de7db
Binoy Patel <binoy.patel@formidable.com>
2022-09-13 16:29:00 -0400

    Adds Select and Input components

---
+++ packages/nextjs/components/Select.tsx
@@ -0,0 +1,81 @@
+import classNames from "classnames";
+import { useSelect } from "downshift";
+import { MdOutlineKeyboardArrowDown, MdOutlineKeyboardArrowUp } from "react-icons/md";
+
+interface Option {
+  title: string;
+  value: string;
+}
+
+export interface Props {
+  options: Option[];
+  label: string;
+  placeholder: string;
+}
+
+const itemToString = (item: Option | null) => (item ? item.title : "");
+
+export function Select({ label, placeholder, options }: Props) {
+  const { isOpen, selectedItem, getToggleButtonProps, getLabelProps, getMenuProps, highlightedIndex, getItemProps } =
+    useSelect({
+      items: options,
+      itemToString,
+    });
+
+  return (
+    <div>
+      <div className="w-72 flex flex-col gap-1">
+        <label className="text-body-reg text-blue mb-2" {...getLabelProps()}>
+          {label}
+        </label>
+        <button
+          aria-label="toggle menu"
+          className="px-4 py-2 border border-blue text-blue rounded-lg hover:bg-blue/20 flex justify-between"
+          type="button"
+          {...getToggleButtonProps()}
+        >
+          <span>{selectedItem ? selectedItem.title : placeholder}</span>
+          <span>{isOpen ? <MdOutlineKeyboardArrowUp size={20} /> : <MdOutlineKeyboardArrowDown size={20} />}</span>
+        </button>
+      </div>
+      <ul
+        {...getMenuProps()}
+        className={classNames(
+          "absolute",
+          "w-72",
+          "mt-4",
+          "shadow-md",
+          "max-h-80",
+          "border",
+          "border-blue",
+          "rounded",
+          "overflow-auto",
+          {
+            hidden: !isOpen,
+          }
+        )}
+      >
+        {isOpen &&
+          options.map((item, index) => (
+            <li
+              className={classNames(
+                "first:pt-2",
+                "last:pb-2",
+                "pt-3",
+                "px-4",
+                "flex",
+                "flex-col",
+                "text-blue",
+                highlightedIndex === index && "bg-blue text-yellow",
+                selectedItem === item && "font-bold"
+              )}
+              key={`${item.value}${index}`}
+              {...getItemProps({ item, index })}
+            >
+              <span>{item.title}</span>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+}
