76a5ecdfc846f685d166e60812049c923d9ad9e4
Binoy Patel <binoy.patel@formidable.com>
2022-09-15 12:24:14 -0400

    Update Design for Header
    - Add Logo
    - Hide Blog navigation closes #78

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import React from "react";
-import { MdShoppingCart } from "react-icons/md";
+import { FiShoppingCart } from "react-icons/fi";
 
 import { useCart } from "./CartContext";
 import { Search } from "./Search";
@@ -20,17 +20,17 @@ export const Header = () => {
   const { cartTotal } = useCart();
 
   return (
-    <div className="h-20 flex items-center justify-between bg-white px-6 border-b shadow">
+    <div className="h-28 flex items-center justify-between px-6 border-b shadow">
       <div className="flex items-center">
-        <h2 className="text-2xl font-bold">
-          <Link href="/">
-            <a>NextJs Ecom</a>
-          </Link>
-        </h2>
+        <Link href="/">
+          <a>
+            <img src="/Logo.svg" alt="Formidable Boulangerie" className="h-10" />
+          </a>
+        </Link>
         <nav className="ml-8">
           {NAV_ITEMS.map(({ href, label }) => (
             <Link href={href} key={label}>
-              <a className="text-gray-600 text-xl hover:text-gray-900 mr-2">{label}</a>
+              <a className="text-blue text-body-reg mr-4 last:mr-0">{label}</a>
             </Link>
           ))}
         </nav>
@@ -38,8 +38,10 @@ export const Header = () => {
       <div className="flex items-center">
         <Search />
         <Link href="/cart">
-          <a className="flex items-center">
-            <MdShoppingCart size={30} /> <span className="text-gray-600 text-xl">{cartTotal}</span>
+          <a className="flex items-center text-blue text-body-reg">
+            <span>Cart</span>
+            <FiShoppingCart size={24} className="mx-2" />
+            <span>{cartTotal}</span>
           </a>
         </Link>
       </div>
