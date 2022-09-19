7fb9045f3fcbd7a897eec7dc30bf0092e4681930
Sawyer Burnett <sawyerburnett@gmail.com>
2022-09-19 13:12:29 -0700

    add web vitals

--- packages/nextjs/pages/_app.tsx
+++ packages/nextjs/pages/_app.tsx
@@ -3,7 +3,7 @@ import "../styles/global.css";
 import { CartProvider } from "../components/CartContext";
 import { Layout } from "../components/Layout";
 
-import type { AppProps } from "next/app";
+import type { AppProps, NextWebVitalsMetric } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -13,6 +13,16 @@ function MyApp({ Component, pageProps }: AppProps) {
       </Layout>
     </CartProvider>
   );
+}
+
+/**
+ * A NextJs built-in function which allows for logging of the web-vitals
+ * more info: https://nextjs.org/docs/advanced-features/measuring-performance
+ * Web Vitals: https://web.dev/vitals/
+ * @param metric
+ */
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  console.log(metric);
 }
 
 export default MyApp;
