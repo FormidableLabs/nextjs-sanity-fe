4c664e514f42248a210d83dbdeab2b31e7adcb5f
Grant Sander <gksander93@gmail.com>
2022-09-27 12:08:51 -0500

    Card image fit

--- packages/nextjs/components/Card.tsx
+++ packages/nextjs/components/Card.tsx
@@ -24,10 +24,11 @@ export const Card: React.FC<CardProps> = ({ to, subTitle, title, price, imagePro
       <a className={`flex flex-col justify-center text-blue ${className}`}>
         <Image
           className="rounded-2xl"
-          width={imageProps.width ?? 400}
-          height={imageProps.height ?? 400}
+          width={imageProps.width ?? 500}
+          height={imageProps.height ?? 500}
           src={imageProps.src}
           alt={imageProps.alt}
+          objectFit="cover"
         />
         <h2 className="text-h5 font-medium mt-4 mb-1">{title}</h2>
         {price && <span className="text-eyebrow font-bold">${price}</span>}
