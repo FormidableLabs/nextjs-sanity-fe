49577263f8ccb225d9d414fbf6de3b68fd1a9ba4
Grant Sander <gksander93@gmail.com>
2022-09-28 10:34:41 -0500

    Remove unreachable code

--- packages/nextjs/components/Pagination.tsx
+++ packages/nextjs/components/Pagination.tsx
@@ -74,54 +74,6 @@ export const Pagination = ({ onPageChange, pageCount = 1, currentPage = 1 }: Pag
       </MaybeDisabledLink>
     </nav>
   );
-
-  return (
-    <nav className="border-t border-gray-200 px-4 flex items-center justify-between sm:px-0">
-      <div className="-mt-px w-0 flex-1 flex">
-        {currentPage > 1 ? (
-          <Link href={prevUrlObj} passHref aria-disabled>
-            <a className="border-t-2 border-transparent pt-4 pr-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300">
-              <FaChevronLeft className="mr-3 h-5 w-5 text-gray-400" aria-hidden="true" />
-              Previous
-            </a>
-          </Link>
-        ) : (
-          <span className="border-t-2 border-transparent pt-4 pr-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300">
-            <FaChevronLeft className="mr-3 h-5 w-5 text-gray-400" aria-hidden="true" />
-            Previous
-          </span>
-        )}
-      </div>
-      <div className="hidden md:-mt-px md:flex">
-        {totalPages.map((page) => (
-          <Link key={`page-${page}`} href={page === 1 ? baseUrlObj : getUrlObjWithPage(page)} passHref>
-            <a
-              onClick={(e) => handlePageChanged(e, page)}
-              className={page == currentPage ? pageSelectedClass : pageClass}
-              aria-current={page === currentPage ? "page" : "false"}
-            >
-              {page}
-            </a>
-          </Link>
-        ))}
-      </div>
-      <div className="-mt-px w-0 flex-1 flex justify-end">
-        {currentPage < pageCount ? (
-          <Link href={nextUrlObj} passHref>
-            <a className="border-t-2 border-transparent pt-4 pl-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300">
-              Next
-              <FaChevronRight className="ml-3 h-5 w-5 text-gray-400" aria-hidden="true" />
-            </a>
-          </Link>
-        ) : (
-          <span className="border-t-2 border-transparent pt-4 pl-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300">
-            Next
-            <FaChevronRight className="ml-3 h-5 w-5 text-gray-400" aria-hidden="true" />
-          </span>
-        )}
-      </div>
-    </nav>
-  );
 };
 
 const MaybeDisabledLink = ({
