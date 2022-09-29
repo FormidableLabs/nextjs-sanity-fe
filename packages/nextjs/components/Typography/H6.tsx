0ecf8ad8e66b086196fbd842a50f5c230a3c647c
Binoy Patel <binoy.patel@formidable.com>
2022-09-29 12:14:44 -0400

    Change element to h6

--- packages/nextjs/components/Typography/H6.tsx
+++ packages/nextjs/components/Typography/H6.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
 
 export const H6 = ({ children }: { children: ReactNode }) => {
-  return <h3 className="font-medium text-h6 text-blue">{children}</h3>;
+  return <h6 className="font-medium text-h6 text-blue">{children}</h6>;
 };
