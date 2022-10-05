9a84c5271bf41d7d208f0a1c5df9888aeb24154e
scottrippey <scott.rippey@formidable.com>
2022-10-05 01:53:08 -0600

    e2e(ssr): run Next app from within Cypress

--- packages/tests-e2e/cypress.config.ts
+++ packages/tests-e2e/cypress.config.ts
@@ -1,14 +1,20 @@
 import { defineConfig } from "cypress";
 import { interceptSSR } from "./cypress/tasks/interceptSSR/task";
 
+import { startNextServer } from "../nextjs/next-server";
+
+const PORT = 9999;
+
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3000",
-    setupNodeEvents(on, config) {
+    baseUrl: `http://localhost:${PORT}`,
+    async setupNodeEvents(on, config) {
       // implement node event listeners here
       on("task", {
         interceptSSR,
       });
+
+      await startNextServer({ port: PORT });
     },
   },
 });
