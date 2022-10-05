6501f017efda1c120f91cd0dd6ba078594a40296
scottrippey <scott.william.rippey@gmail.com>
2022-10-04 21:00:08 -0600

    e2e: improved price formatting

--- packages/nextjs/components/Price.tsx
+++ packages/nextjs/components/Price.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Eyebrow } from "components/Typography/Eyebrow";
+import { currencyFormatter } from "utils/currencyFormatter";
 
 interface Props {
   msrp?: number | null;
@@ -10,15 +11,11 @@ export const Price: React.FC<Props> = ({ msrp, price }) => {
   if (price !== msrp) {
     return (
       <span className="flex gap-x-2">
-        <Eyebrow className="text-red line-through">{formatPrice(msrp ?? 0)}</Eyebrow>
-        <Eyebrow className="">{formatPrice(price ?? 0)}</Eyebrow>
+        <Eyebrow className="text-red line-through">{currencyFormatter.format(msrp ?? 0)}</Eyebrow>
+        <Eyebrow className="">{currencyFormatter.format(price ?? 0)}</Eyebrow>
       </span>
     );
   }
 
-  return <Eyebrow className="">{formatPrice(price ?? 0)}</Eyebrow>;
+  return <Eyebrow className="">{currencyFormatter.format(price ?? 0)}</Eyebrow>;
 };
-
-function formatPrice(price: number) {
-  return `$${price.toFixed(2)}`;
-}
