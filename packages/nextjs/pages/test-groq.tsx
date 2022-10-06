af80372647aaff18858d08a05291ad4e70e8a09e
scottrippey <scott.rippey@formidable.com>
2022-10-06 15:53:08 -0600

    e2e(ssr): added page to test groq

---
+++ packages/nextjs/pages/test-groq.tsx
@@ -0,0 +1,23 @@
+import { GetServerSideProps } from "next";
+import { sanityClient } from "utils/sanityClient";
+import groq from "groq";
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const result = await sanityClient.fetch(groq`
+  	*[_type == "category" ] {
+  	  _id,
+  		name,
+  		'slug': slug.current,
+		}`);
+
+  return { props: { result } };
+};
+
+export default function TestGroq(props: any) {
+  return (
+    <main>
+      <h1>Testing GROQ</h1>
+      <pre>{JSON.stringify(props.result)}</pre>
+    </main>
+  );
+}
