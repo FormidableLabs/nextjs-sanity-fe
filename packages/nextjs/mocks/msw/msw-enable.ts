9d5906ae75c5fe2fb3f435f131816d1591737470
scottrippey <scott.rippey@formidable.com>
2022-10-06 21:52:48 -0600

    e2e(ssr): added 'unsupported Node 18' message

--- packages/nextjs/mocks/msw/msw-enable.ts
+++ packages/nextjs/mocks/msw/msw-enable.ts
@@ -3,18 +3,24 @@ import { groqHandlers } from "./groq/handlers";
 
 /* eslint-disable @typescript-eslint/no-var-requires */
 
+// See https://github.com/mswjs/interceptors/issues/159
+const mswUnsupported = process.version.startsWith("v18");
+if (mswUnsupported) {
+  throw new Error(`[MSW Mock Server] MSW does not support mocking 'fetch' in Node v18`);
+}
+
 export async function initializeMSW() {
   console.log("[MSW Mock Server] Starting...");
   if (typeof window === "undefined") {
     // Start the Node server
     const { setupServer } = require("msw/node");
     const server = setupServer(...handlers, ...groqHandlers);
-    await server.listen();
+    await server.listen({ onUnhandledRequest: "bypass" });
   } else {
     // Start the Browser server
     const { setupWorker } = require("msw");
     const worker = setupWorker(...handlers, ...groqHandlers);
-    await worker.start();
+    await worker.start({ onUnhandledRequest: "bypass" });
   }
   console.log("[MSW Mock Server] Ready");
 }
