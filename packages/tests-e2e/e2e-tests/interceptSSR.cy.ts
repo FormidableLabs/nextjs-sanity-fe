c08174abb6ee8b2256277efdbab92d27da2595b3
scottrippey <scott.rippey@formidable.com>
2022-10-09 15:25:16 -0600

    e2e(ssr): removed obsolete test

--- packages/tests-e2e/e2e-tests/interceptSSR.cy.ts
+++
@@ -1,9 +0,0 @@
-describe("interceptSSR", () => {
-  it("should be called before visiting a page", async () => {
-    const mockJoke = "What do you give a sick lemon? Lemonaid.";
-
-    cy.interceptSSR("https://icanhazdadjoke.com/", { joke: mockJoke });
-
-    cy.visit("/test-interceptSSR");
-  });
-});
