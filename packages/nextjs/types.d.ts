faf1cf19818956caa13d528ead4b85f1c9010116
scottrippey <scott.rippey@formidable.com>
2022-10-09 23:02:00 -0600

    e2e(ssr): fixed type issues

--- packages/nextjs/types.d.ts
+++ packages/nextjs/types.d.ts
@@ -1,5 +1,6 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
     readonly NEXT_PUBLIC_SANITY_PROJECT_ID: string;
+    readonly NEXT_PUBLIC_API_MOCKING: 'enabled' | undefined;
   }
 }
