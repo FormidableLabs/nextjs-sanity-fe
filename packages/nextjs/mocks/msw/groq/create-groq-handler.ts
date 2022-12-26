5acf55f263e31349d1efe7ac5ef495b1ccfb8e20
scottrippey <scott.rippey@formidable.com>
2022-12-26 14:23:27 -0700

    mocks: added cache for mock GROQ queries

--- packages/nextjs/mocks/msw/groq/create-groq-handler.ts
+++ packages/nextjs/mocks/msw/groq/create-groq-handler.ts
@@ -3,6 +3,11 @@ import * as groqJs from "groq-js";
 import type { Dataset } from "./create-dataset";
 
 /**
+ * Note, this cache is never cleaned up, so it will accumulate in size during development.
+ */
+const cache = new Map<string, unknown>();
+
+/**
  * Returns a MSW handler for GROQ queries
  * @param url - The URL of the GROQ server to capture
  * @param getDataset - An array of flattened dataset objects.
@@ -12,9 +17,22 @@ export function createGroqHandler(url: string, getDataset: () => Dataset | Promi
     // Parse the parameters:
     const { query, ...searchParams } = Object.fromEntries(req.url.searchParams.entries());
     const params = parseParams(searchParams);
-    const rawData = await getDataset();
 
-    const result = await executeQuery(rawData, query, params);
+    const dataset = await getDataset();
+
+    // Don't worry ... even though this stringify looks large, it only takes like 1ms
+    const cacheKey = JSON.stringify([dataset, query, params]);
+    const cached = cache.get(cacheKey);
+
+    let result: unknown;
+    if (cached) {
+      result = cached;
+    } else {
+      result = await executeQuery(dataset, query, params);
+
+      cache.set(cacheKey, result);
+    }
+
     // Return the result:
     const body = { result };
     return res(ctx.json(body));
@@ -34,7 +52,7 @@ function parseParams(searchParams: object) {
   );
 }
 
-export async function executeQuery(dataset: Dataset, query: string, params: Record<string, string>) {
+export async function executeQuery(dataset: Dataset, query: string, params: Record<string, string>): Promise<unknown> {
   const parsed = groqJs.parse(query, { params });
   const streamResult = await groqJs.evaluate(parsed, { dataset, params });
   const result = await streamResult.get();
