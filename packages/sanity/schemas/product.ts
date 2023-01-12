cc25c96eb636d1bde3108fcc0729125861ccd35e
Sarah Meyer <sarah.meyer@formidable.com>
2023-01-12 16:32:07 -0700

    fix anonymous inline error from sanity deploy

--- packages/sanity/schemas/product.ts
+++ packages/sanity/schemas/product.ts
@@ -47,28 +47,7 @@ export default {
         {
           name: "productImage",
           title: "Product Image",
-          description: "Images of Products",
-          type: "image",
-          icon: BsFillImageFill,
-          fields: [
-            {
-              name: "name",
-              title: "Name",
-              type: "string",
-              validation: (rule) => rule.required(),
-            },
-            {
-              name: "description",
-              title: "Description",
-              type: "text",
-            },
-          ],
-          preview: {
-            select: {
-              title: "name",
-              media: "asset",
-            },
-          },
+          type: "productImage",
         },
       ],
     },
