39e1fe22e679a8a307ffbef3af581bf6db051f02
Sarah Meyer <sarah.meyer@formidable.com>
2023-01-24 09:59:04 -0700

    fix PDP query

--- packages/nextjs/utils/getProductBySlug.ts
+++ packages/nextjs/utils/getProductBySlug.ts
@@ -2,7 +2,7 @@ import groq from "groq";
 import { sanityClient } from "./sanityClient";
 
 export const getProductBySlug = (slug = "") =>
-  sanityClient.fetch(groq`*[_type == "product" && slug.current == ${slug}]{
+  sanityClient.fetch(groq`*[_type == "product" && slug.current == '${slug}']{
     _id,
     name,
     categories[]->{
