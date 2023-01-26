585567b027c02ab9be4adf3025424cbad3883972
Sarah Meyer <sarah.meyer@formidable.com>
2023-01-26 08:58:28 -0700

    add site settings and remove Variant from content column in studio

--- packages/sanity/schemas/schema.ts
+++ packages/sanity/schemas/schema.ts
@@ -6,17 +6,19 @@ import variant from "./variant";
 import flavour from "./flavour";
 import style from "./style";
 import productImage from "./productImage";
+import siteSettings from "./siteSettings";
 
 // Then import schema types from any plugins that might expose them
 // Then we give our schema to the builder and provide the result to Sanity
 export default [
   /* Your types here! */
-  categoryImage,
   category,
-  product,
-  productImage,
-  variant,
+  categoryImage,
   description,
   flavour,
+  product,
+  productImage,
+  siteSettings,
   style,
+  variant,
 ];
---
+++ packages/sanity/schemas/siteSettings.ts
@@ -0,0 +1,17 @@
+export default {
+  name: "siteSettings",
+  title: "Site Settings",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Site Title",
+      type: "string",
+    },
+    {
+      name: "description",
+      title: "Site Description",
+      type: "text",
+    },
+  ],
+};
