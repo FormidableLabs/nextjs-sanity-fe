9f2f9ef5fe0da49b03a603caa8f0fbbb1c6a60a3
Luis Abarca <luisabarca@gmail.com>
2023-02-02 00:19:35 -0600

    add new Sort and filter toolbar for mobile devices

---
+++ packages/nextjs/views/SortAndFiltersToolbarMobile.tsx
@@ -0,0 +1,26 @@
+import { Button } from "components/Button";
+import { ProductSort } from "components/ProductSort";
+import React from "react";
+import { MdOutlineFilterList } from "react-icons/md";
+
+interface SortAndFiltersToolbarMobileProps {
+  onFiltersClick?: React.MouseEventHandler;
+}
+
+export const SortAndFiltersToolbarMobile: React.FC<SortAndFiltersToolbarMobileProps> = ({ onFiltersClick }) => {
+  return (
+    <div className="my-6 mb-8 w-full inline-flex items-end justify-between md:hidden">
+      <ProductSort as="select" selectClassName="w-44 mr-2" />
+
+      <Button
+        title="Open filters"
+        type="button"
+        variant="primary"
+        leftIcon={<MdOutlineFilterList className="w-5 h-5" />}
+        onClick={onFiltersClick}
+      >
+        Filters
+      </Button>
+    </div>
+  );
+};
