1fb5ea9a7ccb007425b08ef0c36455fcf1b4b5ce
Luis Abarca <luis.abarca@formidable.com>
2023-02-16 09:11:40 -0600

    187 Add filter count to the filters button on smaller screen sizes (#188)
    * add method to get active filters from current route
    
    * add filters count to button
    
    * remove filters count from modal button
    
    * linter happiness

---
+++ packages/nextjs/utils/getFiltersCount.ts
@@ -0,0 +1,26 @@
+import { useRouter } from "next/router";
+import { ParsedUrlQuery } from "querystring";
+import type { FilterGroup } from "./filters";
+import { getFilterGroups } from "./filters";
+
+export const getFiltersCount = (query: ParsedUrlQuery) => {
+  const filters = getFilterGroups();
+
+  const total = filters.reduce((acc: number, { label, value }: FilterGroup) => {
+    const selectedFilters = query[value];
+    const elements = selectedFilters?.length ?? 0;
+
+    // if a single element is selected, type would be a string instead of an array.
+    const totalFilters = typeof selectedFilters === "string" ? (elements > 0 ? 1 : 0) : elements;
+
+    return (acc = acc + totalFilters);
+  }, 0);
+
+  return total;
+};
+
+export const useGetFiltersCount = () => {
+  const router = useRouter();
+
+  return getFiltersCount(router.query);
+};
