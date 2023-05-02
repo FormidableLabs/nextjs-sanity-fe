a586bf791f71f46f292d8444f8d4b22ef68b4f51
Carlos Kelly <me@carlos.dev>
2023-05-02 14:56:26 -0500

    Migrate filters to groqd.

--- packages/nextjs/utils/getFilters.ts
+++ packages/nextjs/utils/getFilters.ts
@@ -1,39 +1,37 @@
-import groq from "groq";
-import { CategoryFilterItem, FlavourFilterItem, StyleFilterItem } from "./groqTypes/ProductList";
-import { sanityClient } from "./sanityClient";
+import { q } from "groqd";
+import { runQuery } from "./sanityClient";
 
 /**
- * Fetches categories for filters.
  * Note that the `count(*[_type=="product"][references(^._id)]) > 0` ensures category is only returned
  *   if at least one product is in the category (e.g., no empty categories)
  * This is a nice feature, but you pay a perf penalty for it. Trade-offs 🤷‍
- * 	(since we've got a strong caching strategy, this penalty is minimized)
+ *  (since we've got a strong caching strategy, this penalty is minimized)
  */
-export const getCategoryFilters = (categorySlug = ""): Promise<CategoryFilterItem[]> =>
-  sanityClient.fetch(groq`
-  	*[_type == "category"][count(*[_type=="product"][references(^._id)]) > 0] {
-  		name,
-  		'slug': slug.current,
-		}`);
+const getFilterForType = (type: string, parentType: string) =>
+  runQuery(
+    q("*")
+      .filter("_type == $type")
+      .filter("count(*[_type==$parentType][references(^._id)]) > 0")
+      .grab$({
+        name: q.string(),
+        slug: q.slug("slug"),
+      }),
+    { type, parentType }
+  );
+
+/**
+ * Fetches categories for filters.
+ */
+export const getCategoryFilters = () => getFilterForType("category", "product");
 
 /**
  * Fetches "styles".
  * Similar trade-off as above.
  */
-export const getStyleFilters = (categorySlug = ""): Promise<StyleFilterItem[]> =>
-  sanityClient.fetch(groq`
-  	*[_type == "style"][count(*[_type=="variant"][references(^._id)]) > 0] {
-  		name,
-  		'slug': slug.current,
-		}`);
+export const getStyleFilters = () => getFilterForType("style", "variant");
 
 /**
  * Fetches flavours.
  * Similar trade-off as above.
  */
-export const getFlavourFilters = (categorySlug = ""): Promise<FlavourFilterItem[]> =>
-  sanityClient.fetch(groq`
-  	*[_type == "flavour"][count(*[_type=="variant"][references(^._id)]) > 0] {
-  		name,
-  		'slug': slug.current,
-		}`);
+export const getFlavourFilters = () => getFilterForType("flavour", "variant");
