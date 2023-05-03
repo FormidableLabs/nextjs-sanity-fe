c4de336071b3ecb8c18c8819a51566230a1a236a
scottrippey <scott.rippey@formidable.com>
2023-05-02 22:19:04 -0600

    feature(app-router): migrated home and categories pages

---
+++ packages/nextjs/app/app/categories/page.tsx
@@ -0,0 +1,54 @@
+"use client";
+import { GetServerSideProps, NextPage } from "next";
+
+import { setCachingHeaders } from "utils/setCachingHeaders";
+import { SanityType } from "utils/consts";
+import { isString, pluralize } from "utils/pluralize";
+import { getAllCategories } from "utils/getAllCategoriesQuery";
+
+import { CategoryList } from "components/CategoryList";
+import { WeDontSellBreadBanner } from "components/WeDontSellBreadBanner";
+import { PageHead } from "components/PageHead";
+import { Breadcrumbs } from "components/Breadcrumbs";
+import { Category } from "utils/groqTypes/ProductList";
+
+interface PageProps {
+  categories: Category[];
+  categoryNames: string;
+}
+
+const CategoriesPage: NextPage<PageProps> = async () => {
+  const { props } = await getServerSideProps({});
+  const { categories, categoryNames } = props;
+  return (
+    <>
+      <PageHead title="Categories" description={`Product categories, including ${categoryNames}.`} />
+      <div>
+        <WeDontSellBreadBanner />
+        <div className="container py-9 text-primary flex flex-col gap-9">
+          <h1 className="text-h1">Categories</h1>
+          <div className="my-2">
+            <Breadcrumbs />
+          </div>
+          <CategoryList items={categories} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+const getServerSideProps = (async ({ res }) => {
+  // setCachingHeaders(res, [SanityType.Category, SanityType.CategoryImage]);
+
+  const categories = await getAllCategories();
+  const categoryNames = pluralize((categories || []).map((cat) => cat.name).filter(isString));
+
+  return {
+    props: {
+      categories,
+      categoryNames,
+    },
+  };
+}) satisfies GetServerSideProps<PageProps>;
+
+export default CategoriesPage;
---
+++ packages/nextjs/app/app/page.tsx
@@ -0,0 +1,124 @@
+"use client";
+import type { Categories, Products } from "utils/groqTypes/ProductList";
+import * as React from "react";
+import { GetServerSideProps, NextPage } from "next";
+import { FiArrowRight } from "react-icons/fi";
+import Link from "next/link";
+import NextImage from "next/legacy/image";
+
+import { setCachingHeaders } from "utils/setCachingHeaders";
+import { localImageLoader } from "utils/localImageLoader";
+import { SanityType } from "utils/consts";
+import { getAllCategories } from "utils/getAllCategoriesQuery";
+import { getRecommendations } from "utils/getRecommendationsQuery";
+
+import featuredImg from "assets/featured-story.jpg";
+import { Button } from "components/Button";
+import { FeaturedList } from "components/FeaturedList";
+import { FeaturedQuote } from "components/FeaturedQuote";
+import { Image } from "components/Image";
+import { PageHead } from "components/PageHead";
+
+interface PageProps {
+  data?: {
+    products: Products;
+    categories: Categories;
+  };
+}
+
+const Home: NextPage<PageProps> = async () => {
+  const { props } = await getServerSideProps({});
+  const { data } = props;
+  return (
+    <>
+      <PageHead
+        title="Home"
+        description="Formidable Boulangerie home page. A showcase of Next.js, Sanity CMS, and Fastly CDN."
+      />
+      <section className="container">
+        <div className="flex justify-between items-center py-9">
+          <div className="max-w-[600px]">
+            <h1 className="text-primary text-h1">Formidable breads for your daily life.</h1>
+            <Link href="/products" legacyBehavior>
+              <Button as="a" variant="secondary" className="inline-flex items-center mt-6">
+                <FiArrowRight size={24} className="mr-2" /> Show now
+              </Button>
+            </Link>
+          </div>
+
+          <span className="hidden sm:block">
+            <Image
+              width={600}
+              height={600}
+              className="rounded-2xl"
+              src={data?.products[0].images?.[0] ?? ""}
+              alt={data?.products[0].name ?? ""}
+            />
+          </span>
+        </div>
+      </section>
+
+      <TitleBanner>Our bestsellers</TitleBanner>
+      <section className="container py-9 flex flex-col gap-9">
+        <FeaturedList items={data?.products} />
+        <Link href="/products" legacyBehavior>
+          <Button as="a" variant="primary" className="w-full inline-block text-center">
+            Show all breads
+          </Button>
+        </Link>
+      </section>
+
+      <FeaturedQuote />
+
+      <TitleBanner>Top categories</TitleBanner>
+      <section className="container py-9">
+        <FeaturedList items={data?.categories.slice(0, 3)} />
+      </section>
+
+      <section className="py-9 bg-primary w-full">
+        <div className="container flex flex-col sm:flex-row gap-9 items-center">
+          <div className="w-full sm:max-w-[320px] md:max-w-[586px]">
+            <NextImage
+              src={featuredImg}
+              loader={localImageLoader}
+              layout="intrinsic"
+              alt="featured image"
+              className="rounded-full overflow-hidden aspect-square"
+            />
+          </div>
+          <div className="flex-1 text-secondary">
+            <h6 className="text-h6 font-jeanLuc font-bold">Stories</h6>
+            <h2 className="text-h2">Formidable Baker: Felicity Tai</h2>
+            <Button as="a" variant="tertiary" className="inline-flex items-center mt-6">
+              <FiArrowRight size={24} className="mr-2" /> Show now
+            </Button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+const TitleBanner = ({ children }: React.PropsWithChildren) => (
+  <div className="border-y-2 border-y-primary py-6">
+    <h4 className="text-h4 text-primary container">{children}</h4>
+  </div>
+);
+
+const getServerSideProps = (async ({ res }) => {
+  // setCachingHeaders(res, [SanityType.Category, SanityType.CategoryImage]);
+
+  const categories = await getAllCategories();
+  const products = await getRecommendations();
+
+  return {
+    props: {
+      data: {
+        products,
+        categories,
+      },
+    },
+  };
+}) satisfies GetServerSideProps<PageProps>;
+
+export default Home;
