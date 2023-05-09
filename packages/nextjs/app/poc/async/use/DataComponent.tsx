73a4b1ae1b013e990012e81857e6bb7768d1f920
scottrippey <scott.rippey@formidable.com>
2023-05-09 00:17:00 -0600

    feature(app-router): use + cache

--- packages/nextjs/app/poc/async/use/DataComponent.tsx
+++ packages/nextjs/app/poc/async/use/DataComponent.tsx
@@ -1,11 +1,12 @@
-import { use, useMemo } from "react";
+import { use, cache } from "react";
+// @ts-expect-error -- No types
+import memoize from "lodash.memoize";
 import { fetchSlowData } from "../common/fetchSlowData";
 
+const fetchSlowDataCache: typeof fetchSlowData =
+  typeof window === "undefined" ? memoize(fetchSlowData) : cache(fetchSlowData);
+
 export function DataComponent() {
-  const data = use(
-    useMemo(async () => {
-      return fetchSlowData({});
-    }, [])
-  );
+  const data = use(fetchSlowDataCache({}));
   return <p>{data}</p>;
 }
