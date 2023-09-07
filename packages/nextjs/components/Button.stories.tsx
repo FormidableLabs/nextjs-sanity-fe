f34b6d7ec19c344e944ec9dbb86dd7f42229c838
scottrippey <scott.rippey@formidable.com>
2023-09-07 11:31:42 -0600

    dev(storybook): created stories for Button

---
+++ packages/nextjs/components/Button.stories.tsx
@@ -0,0 +1,49 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Button } from "./Button";
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  args: {
+    children: <>Button</>,
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Variants: Story = {
+  render() {
+    return (
+      <div className="flex flex-col items-start p-2 gap-2 bg-gray">
+        <Button variant="primary">Primary</Button>
+        <Button variant="secondary">Secondary</Button>
+        <Button variant="tertiary">Tertiary</Button>
+        <Button variant="text">Text</Button>
+      </div>
+    );
+  },
+};
+
+export const Primary: Story = {
+  args: {
+    variant: "primary",
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    variant: "secondary",
+  },
+};
+
+export const Tertiary: Story = {
+  args: {
+    variant: "tertiary",
+  },
+};
+
+export const Text: Story = {
+  args: {
+    variant: "text",
+  },
+};
