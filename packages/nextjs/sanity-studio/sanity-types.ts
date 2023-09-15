91f33e6bdf9693bf9c412010279e4a0132350e4b
scottrippey <scott.rippey@formidable.com>
2023-09-15 14:17:07 -0600

    types: manually typed the schema, for performance

--- packages/nextjs/sanity-studio/sanity-types.ts
+++ packages/nextjs/sanity-studio/sanity-types.ts
@@ -1,6 +1,33 @@
-import { InferSchemaValues } from "@sanity-typed/types";
-
-import config from "./sanity.config";
+// import { InferSchemaValues } from "@sanity-typed/types";
+import { _referenced } from "@sanity-typed/types";
+// import config from "./sanity.config";
 
 /** Typescript type of all types! */
-export type SanityValues = InferSchemaValues<typeof config>;
+// export type SanityValues = InferSchemaValues<typeof config>;
+
+// The `InferSchemaValues` is like SUPER slow, so here's a snapshot:
+export type SanityValuesForTests = {
+  product: {
+    slug: { _type: "slug"; current: string };
+    name: string;
+    description: string;
+    categories?: Array<{
+      _type: "reference";
+      _ref: string;
+      [_referenced]: "category";
+    }>;
+    variants?: Array<{
+      _type: "reference";
+      _ref: string;
+      [_referenced]: "variant";
+    }>;
+  };
+  category: {
+    slug: { _type: "slug"; current: string };
+    name: string;
+    description: string;
+  };
+  variant: {
+    name: string;
+  };
+};
