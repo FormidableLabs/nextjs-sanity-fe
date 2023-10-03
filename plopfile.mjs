7542679f884483c135c33928f178a029b668f2f5
Scott Rippey <scottrippey@users.noreply.github.com>
2023-10-03 15:23:42 -0600

    Storybook: sort files in stories generator (#205)

--- plopfile.mjs
+++ plopfile.mjs
@@ -24,6 +24,7 @@ export default function (plop) {
               nodir: true,
               nocase: true,
             })
+            .sort((a, b) => a.localeCompare(b))
             .map((file) => {
               const COMPONENT = {
                 FILE: file,
