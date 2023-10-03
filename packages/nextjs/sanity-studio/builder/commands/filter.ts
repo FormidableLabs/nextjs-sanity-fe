f8e7f6b8589ec46697735b00ccc2c7a838940205
scottrippey <scott.rippey@formidable.com>
2023-10-03 06:15:59 -0600

    types(groq-builder): implemented `filterBy`

--- packages/nextjs/sanity-studio/builder/commands/filter.ts
+++ packages/nextjs/sanity-studio/builder/commands/filter.ts
@@ -1,9 +1,13 @@
 import { GroqBuilder } from "../groq-builder";
 import { ArrayItem } from "../type-utils";
-import { RootConfig } from "../common-types";
+import { RootConfig, StringKeys } from "../common-types";
 
 declare module "../groq-builder" {
   export interface GroqBuilder<TScope, TRootConfig extends RootConfig> {
+    filterBy<TKey extends keyof ArrayItem<TScope>, TValue extends ArrayItem<TScope>[TKey]>(
+      filterString: `${StringKeys<TKey>} == "${Extract<TValue, string>}"`
+    ): GroqBuilder<Array<Extract<ArrayItem<TScope>, { [P in TKey]: TValue }>>, TRootConfig>;
+
     filter<TScopeNew extends TScope = TScope>(filterString?: string): GroqBuilder<TScopeNew, TRootConfig>;
     filterByType<TType extends Extract<ArrayItem<TScope>, { _type: any }>["_type"]>(
       type: TType
@@ -12,10 +16,12 @@ declare module "../groq-builder" {
 }
 
 GroqBuilder.implement({
+  filterBy(this: GroqBuilder<any, any>, filterString) {
+    return this.extend(`[${filterString}]`, null);
+  },
   filter(this: GroqBuilder<any, any>, filterString = "") {
     return this.extend(`[${filterString}]`, null);
   },
-
   filterByType(this: GroqBuilder<any, any>, type) {
     return this.extend(`[_type == '${type}']`, null);
   },
