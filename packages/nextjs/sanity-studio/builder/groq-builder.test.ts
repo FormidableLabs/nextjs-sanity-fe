db21300ff937e52697b484b81e45254a3bf937ae
scottrippey <scott.rippey@formidable.com>
2023-10-03 11:13:55 -0600

    types(groq-builder): updated test with `getProductBySlug` implementation

--- packages/nextjs/sanity-studio/builder/groq-builder.test.ts
+++ packages/nextjs/sanity-studio/builder/groq-builder.test.ts
@@ -1,40 +1,63 @@
 import { createGroqBuilder } from "./groq-builder";
 import { SchemaConfig } from "../sanity-types";
+import { expectType } from "./test-utils/expectType";
+import { ExtractScope } from "./common-types";
+
 const q = createGroqBuilder<SchemaConfig>();
 
-describe("filterByType", () => {
-  it("", () => {
-    const res = q.field("*").filterByType("category");
-    expect(res).toMatchObject({
-      query: "*[_type == 'category']",
-    });
-  });
-});
+describe("", () => {
+  const cats = q.star.filterByType("product").grabOne("categories");
+  cats.TScope;
+  cats.TScopeItem;
 
-describe("field", () => {
-  it("", () => {
-    const res = q.field("*");
-    expect(res).toMatchObject({
-      query: "*",
-    });
-  });
-  it("", () => {
-    const res = q.field("category");
-    expect(res).toMatchObject({
-      query: "category",
-    });
-  });
-  it("", () => {
-    // @ts-expect-error ---
-    const res = q.field("INVALID");
-  });
-});
+  it("getProductBySlug", () => {
+    const res = q.star
+      .filterByType("product")
+      .filter("slug.current == $slug")
+      .grab((q) => ({
+        _id: true,
+        name: true,
+        categories: q.grabOne("categories").filter().deref().grab({
+          name: true,
+        }),
+        slug: q.slug("slug"),
+        variants: q
+          .grabOne("variants")
+          .filter()
+          .deref()
+          .grab((q) => ({
+            _id: true,
+            name: true,
+            msrp: true,
+            price: true,
+            slug: q.slug("slug"),
+            style: q.grabOne("style").filter().deref().grab({
+              _id: true,
+              name: true,
+            }),
+          })),
+      }));
 
-describe("deref", () => {
-  it("", () => {
-    const res = q.field("category").deref();
-    expect(res).toMatchObject({
-      query: "category->",
-    });
+    expectType<ExtractScope<typeof res>>().toStrictEqual<
+      Array<{
+        _id: string;
+        name: string;
+        categories: Array<{
+          name: string;
+        }>;
+        slug: string;
+        variants: Array<{
+          _id: string;
+          name: string;
+          msrp: number;
+          price: number;
+          slug: string;
+          style: Array<{
+            _id: string;
+            name: string;
+          }>;
+        }>;
+      }>
+    >();
   });
 });
