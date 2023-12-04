822bf3d43f3f9b9f8b2f7dc7f6685d7905b0e6a3
Nathan Kluth <nlkluth@gmail.com>
2023-12-04 10:54:48 -0700

    slug server page

---
+++ packages/nextjs/app/products/[slug]/page.tsx
@@ -0,0 +1,25 @@
+import ProductsPage from "app/migration/products/[slug]";
+import { getProductBySlug } from "utils/getProductBySlug";
+import { getRecommendations } from "utils/getRecommendationsQuery";
+import { isSlug } from "utils/isSlug";
+
+// See: https://nextjs.org/docs/app/api-reference/file-conventions/page
+type RouteSearchParams = { [key: string]: string | string[] | undefined };
+
+const getData = async ({ searchParams }: { searchParams: RouteSearchParams }) => {
+  const { slug } = searchParams;
+
+  const products = await getProductBySlug(isSlug(slug) ? slug : "");
+  const recommendations = await getRecommendations();
+
+  return {
+    products,
+    recommendations,
+  };
+};
+
+export default async function Page({ searchParams }: { searchParams: RouteSearchParams }) {
+  const data = await getData({ searchParams });
+
+  return <ProductsPage data={data} />;
+}
